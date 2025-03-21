435ebd71524e85d34f77d6879b996c389de5ca7b
JEANETMASE <jeanetmase@gmail.com>
2025-03-21 23:56:35 +0200

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
-function refreshWeather(response) {
-  let tempratureElement = document.querySelector("#temperature");
-  let temperature = response.data.tempareture.current;
-
-  cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = Math.round(temperature);
-}
-
-function searchCity(city) {
-  let apiKey = "b7o9a4at99230450582332933f8140b0";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&`;
-  axios.get(apiUrl).then(refreshWeather);
-}
-
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
-
-  searchCity(searchInput.value);
+  cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
