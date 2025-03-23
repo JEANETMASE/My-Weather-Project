86fdd85d86bf3440b234a433bf79013b656575d3
JEANETMASE <jeanetmase@gmail.com>
2025-03-23 22:44:09 +0200

    Inject functionality to the api and updated the CSS.

--- src/index.js
+++ src/index.js
@@ -55,33 +55,51 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "wed", "Thurs", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "b7o9a4at99230450582332933f8140b0";
-  let apiurl = `https://api.shecodes.io/weather/v1/forecast?query=$(city)&key=$(apiKey)&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
-function dispalyForecast(response) {
+function displayForecast(response) {
   console.log(response.data);
-  let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Tue", "Wed", "Thurs", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="weather-forecast-day">
-                    <div class="weather-forecast-date">${day}</div>
-                    <div class="weather-forecast-icon">&#9729;</div>
+                    <div class="weather-forecast-date">${formatDay(
+                      day.time
+                    )}</div>
+                    
+                    <img src="${
+                      day.condition.icon_url
+                    }" class="weather-forecast-icon" />
+                    
                     <div class="weather-forecast-temperatures">
-                        <div class="weather-forecast-temperature"><strong>15°</strong></div>
-                        <div class="weather-forecast-temperature">9°</div>
+                        <div class="weather-forecast-temperature"><strong>${Math.round(
+                          day.temperature.maximum
+                        )}°</strong></div>
+                        <div class="weather-forecast-temperature">${Math.round(
+                          day.temperature.minimum
+                        )}°</div>
                     </div>
                 </div>
                 `;
+    }
   });
+  let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
 
@@ -89,4 +107,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Bloemfontein");
-dispalyForecast();
